a1efde4f8141f4dd8f0792043cb7a693849e348e
Zeng-Lecheng <50188852+Zeng-Lecheng@users.noreply.github.com>
2023-03-04 14:31:31 -0500

    [Add] Add 4-layer CNN and A-softmax

--- model.py
+++ model.py
@@ -1,6 +1,120 @@
-import torch.nn as nn
 import torch
+import torch.nn as nn
+from torch import pi, cos
+from torch.autograd import Variable
+from torch.nn.parameter import Parameter
+
+
+def phi(theta, m):
+    k = theta // (pi / m)
+    return (-1) ** k * cos(m * theta) - 2 * k
+
+class AngleLinear(nn.Module):
+    def __init__(self, in_features, out_features, m=4, phiflag=True):
+        super(AngleLinear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.Tensor(in_features,out_features))
+        self.weight.data.uniform_(-1, 1).renorm_(2,1,1e-5).mul_(1e5)
+        self.phiflag = phiflag
+        self.m = m
+        self.mlambda = [
+            lambda x: x**0,
+            lambda x: x**1,
+            lambda x: 2*x**2-1,
+            lambda x: 4*x**3-3*x,
+            lambda x: 8*x**4-8*x**2+1,
+            lambda x: 16*x**5-20*x**3+5*x
+        ]
+
+    def forward(self, input):
+        x = input   # size=(B,F)    F is feature len
+        w = self.weight # size=(F,Classnum) F=in_features Classnum=out_features
+
+        ww = w.renorm(2,1,1e-5).mul(1e5)
+        xlen = x.pow(2).sum(1).pow(0.5) # size=B
+        wlen = ww.pow(2).sum(0).pow(0.5) # size=Classnum
+
+        cos_theta = x.mm(ww) # size=(B,Classnum)
+        cos_theta = cos_theta / xlen.view(-1,1) / wlen.view(1,-1)
+        cos_theta = cos_theta.clamp(-1,1)
+
+        if self.phiflag:
+            cos_m_theta = self.mlambda[self.m](cos_theta)
+            theta = Variable(cos_theta.data.acos())
+            k = self.m * theta // pi
+            n_one = k * 0. - 1
+            phi_theta = (n_one**k) * cos_m_theta - 2*k
+        else:
+            theta = cos_theta.acos()
+            phi_theta = phi(theta, self.m)
+            phi_theta = phi_theta.clamp(-1*self.m, 1)
+
+        cos_theta = cos_theta * xlen.view(-1,1)
+        phi_theta = phi_theta * xlen.view(-1,1)
+        output = (cos_theta,phi_theta)
+        return output # size=(B,Classnum,2)
 
 
 class Net(nn.Module):
-    pass
+    def __init__(self, num_class: int, get_feature: bool = False):
+        super().__init__()
+        self.num_class = num_class
+        self.get_feature = get_feature
+
+        self.conv1 = nn.Conv2d(3, 64, 3, stride=2)
+        self.conv2 = nn.Conv2d(64, 128, 3, stride=2)
+        self.conv3 = nn.Conv2d(128, 256, 3, stride=2)
+        self.conv4 = nn.Conv2d(256, 512, 3, stride=2)
+
+        self.fc1 = nn.Linear(512, 512)
+        self.fc2 = AngleLinear(512, self.num_class)
+
+    def forward(self, x):
+        x = torch.relu(self.conv1(x))
+        x = torch.relu(self.conv2(x))
+        x = torch.relu(self.conv3(x))
+        x = torch.relu(self.conv4(x))
+
+        x = torch.flatten(x, start_dim=1)
+        x = self.fc1(x)
+
+        if self.get_feature:
+            return x
+        else:
+            return self.fc2(x)
+
+
+class AngleLoss(nn.Module):
+    def __init__(self, gamma=0):
+        super(AngleLoss, self).__init__()
+        self.gamma   = gamma
+        self.it = 0
+        self.LambdaMin = 5.0
+        self.LambdaMax = 1500.0
+        self.lamb = 1500.0
+
+    def forward(self, input, target):
+        self.it += 1
+        cos_theta,phi_theta = input
+        target = target.view(-1,1) #size=(B,1)
+
+        index = cos_theta.data * 0.0 #size=(B,Classnum)
+        index.scatter_(1,target.data.view(-1,1),1)
+        index = index.byte()
+        index = Variable(index)
+
+        self.lamb = max(self.LambdaMin,self.LambdaMax/(1+0.1*self.it ))
+        output = cos_theta * 1.0 #size=(B,Classnum)
+        output[index] -= cos_theta[index]*(1.0+0)/(1+self.lamb)
+        output[index] += phi_theta[index]*(1.0+0)/(1+self.lamb)
+
+        logpt = torch.log_softmax(output, dim=1)
+        logpt = logpt.gather(1,target)
+        logpt = logpt.view(-1)
+        pt = Variable(logpt.data.exp())
+
+        loss = -1 * (1-pt)**self.gamma * logpt
+        loss = loss.mean()
+
+        return loss
